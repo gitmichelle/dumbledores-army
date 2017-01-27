1e9a4b0465100629f5269412e8a00e446a845704
Michelle Llaguno <michellellaguno@gmail.com>
2017-01-26 23:18:31 -0800

    add some members

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../member.model';
 
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
   styleUrls: ['./members.component.css']
 })
-export class MembersComponent implements OnInit {
+export class MembersComponent {
+  members: Member[] = [
+   new Member("Harry Potter", "Gryffindor",
+       "stag", 1),
+   new Member("Ron Wealey", "Gryffindor",
+       "Jack Russell Terrier", 2),
+   new Member("Hermione Granger", "Gryffindor",
+       "Otter", 3),
+   new Member("Hannah Abbott", "Hufflepuff", "none", 4),
+   new Member("Lavender Brown", "Gryffindor", "house rat", 5),
+   new Member("Katie Bell", "Gryffindor", "cougar", 6)
+ ];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
