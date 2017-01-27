7adced5418a49b7e8d29b3f58e66067339dd97e3
Michelle Llaguno <michellellaguno@gmail.com>
2017-01-26 23:00:33 -0800

    add about page and about route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,17 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AlohamoraComponent } from './alohamora/alohamora.component';
+import { AboutComponent }   from './about/about.component';
 
 
 const appRoutes: Routes = [
   {
     path: '',
     component: AlohamoraComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
   }
 
 ];
